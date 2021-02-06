72a1fd3af6833c58005fae58db541ad3dcd227df
RatihEl <ratiheldina1234@gmail.com>
2021-02-06 15:19:04 +0700

    Initial commit

---
+++ lib/dummy.dart
@@ -0,0 +1,122 @@
+import 'package:tourism_app1/tourisms.dart';
+
+var tourismsList = [
+  Tourisms(name: 'Westminer',
+      address: 'London, Inggris',
+      description: 'City of Westminster adalah borough di London Dalam yang didirikan pada tahun 1965 ketika London Raya terbentuk. Pada awalnya, Westminster diberi status kota, yang sebelumnya telah dipegang oleh Borough Metropolitan yang lebih kecil dari Westminster',
+      scedule: '06.00 -  07.00',
+      ticket:'200.000',
+      photo:'asset/img/westminster.jpg',
+      imgurl: [
+       'https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/London-Eye....jpg', 'https://tempatwisataunik.com/wp-content/uploads/2016/03/Natural-History-Museum-1.jpg']
+  ),
+  Tourisms(name: 'British museum',
+      address: 'London, Inggris',
+      scedule: '06.00 -  07.00',
+      description: 'Bristish Museum merupakan tempat wisata pertama yang dapat dikunjungi apabila Anda datang ke Inggris. British Museum merupakan museum tertua di dunia yang memiliki koleksi tidak kurang dari 6 juta koleksi, di mana koleksi tersebut mencakup 1,8 juta tahun peradaban. Contohnya ialah Mummified Cat (mumi kucing) yang berasal dari Abydos dan tercacat sejak 30 SM.',
+      ticket:'200.000',
+      photo:'asset/img/british_museum.jpg',
+      imgurl: [
+        'https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/London-Eye....jpg', 'https://tempatwisataunik.com/wp-content/uploads/2016/03/Natural-History-Museum-1.jpg']
+  ),
+  Tourisms(name: 'london Eye',
+      address: 'London, Inggris',
+      scedule: '06.00 -  07.00',
+      ticket:'200.000',
+      description: 'London Eye merupakan tempat wisata kedua yang dapat dikunjungi apabila Anda datang ke Inggris. London Eye merupakan sebuah kincir angina raksasa terbesar kedua yang pernah dibangun di dunia. Kincir raksasa ini terletak di seberang Houses of Parliament, South Bank. Meskipun antrian untuk bisa menaiki wahana ini cukup panjang setiap harinya, namun Anda dijamin tidak akan menyesal apabila ingin mencoba menaikinya apabila datang ke London, Inggris.',
+      photo:'asset/img/london_eye.jpg',
+      imgurl: [
+        'https://tempatwisataunik.com/wp-content/uploads/2016/03/London-Eye....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/London-Eye....jpg', 'https://tempatwisataunik.com/wp-content/uploads/2016/03/Natural-History-Museum-1.jpg']
+  ),
+  Tourisms(name: 'Natural History Museum',
+      address: 'London, Inggris',
+      scedule: '06.00 -  07.00',
+      ticket:'200.000',
+      description: 'Natural History Museum merupakan tempat wisata ketiga yang dapat dikunjungi apabila Anda datang ke Inggris. Museum ini terkenal dengan koleksinya yang sangat besar dan beragam. Terdapat sekitar 70 juta specimen yang ditampilkan, yang mana koleksi-koleksi tersebut berasal dari barang-barang yang dibawa oleh Charles Darwin dan ahli botani Kapten Cook, Joseph Bank. Selain itu, ada juga berbagai aktraksi khusus yang disiapkan untuk anak-anak, seperti koleksi dinosaurus.',
+      photo:'asset/img/Natural_History_Museum.jpg',
+      imgurl: [
+        'https://tempatwisataunik.com/wp-content/uploads/2016/03/Natural-History-Museum-1.jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/London-Eye....jpg', 'https://tempatwisataunik.com/wp-content/uploads/2016/03/Natural-History-Museum-1.jpg']
+  ),
+  Tourisms(name: 'Buckingham_Palace',
+      address: 'London, Inggris',
+      scedule: '06.00 -  07.00',
+      ticket:'200.000',
+      description: 'Tempat wisata yang satu ini merupakan kediaman ratu yang sangat terkenal di Inggris. Yang mana, Anda dapat menyaksikan pergantian penjaga istana dengan seragam merah dan topi tingginya yang terbuat dari bulu beruang pada pukul 11.00 setiap hari atau pukul 10.00 pada hari minggu dan hari-hari musim dingin.' ,
+      photo:'asset/img/Buckingham_Palace.jpg',
+      imgurl: [
+        'https://tempatwisataunik.com/wp-content/uploads/2016/03/Buckingham-Palace....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/London-Eye....jpg', 'https://tempatwisataunik.com/wp-content/uploads/2016/03/Natural-History-Museum-1.jpg']
+  ),
+  Tourisms(name: 'Tower Of London',
+      address: 'London, Inggris',
+      scedule: '06.00 -  07.00',
+      ticket:'200.000',
+      description: 'Benteng yang terletak di sisi sungai ini dikenang sebagai penjara, di mana Henry VIII menghukum mati dua istrinya, yaitu Katharine Howard (1542) dan Anne Boleyn (1536). Di benteng ini Anda juga dapat menjumpai 6 ekor gagak yang dipelihara oleh Ravenmaster. Bahkan pepatah Inggris mengatakan bahwa ketika gagak meninggalkan menara, maka bangunan ini dan monarki akan runtuh.',
+      photo:'asset/img/tower_oflondon.jpg',
+      imgurl: [
+        'https://tempatwisataunik.com/wp-content/uploads/2016/03/Tower-of-London-1.jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/London-Eye....jpg', 'https://tempatwisataunik.com/wp-content/uploads/2016/03/Natural-History-Museum-1.jpg']
+  ),
+  Tourisms(name: 'Picasilly Circus',
+      address: 'London, Inggris',
+      scedule: '06.00 -  07.00',
+      ticket:'200.000',
+      description: 'Piccadilly Circus merupakan tempat wisata ketujuh yang dapat dikunjungi apabila Anda datang ke Inggris. Piccadilly Circus merupakan kawasan persimpangan yang sangat ramai di Inggris. Tempat ini sangat cocok bagi Anda yang ingin berjalan-jalan santai sambil berbaur dengan masyarakat Inggris.',
+      photo:'asset/img/piccadilly_circus.jpg',
+      imgurl: [
+        'https://tempatwisataunik.com/wp-content/uploads/2016/03/Piccadilly-Circus.jpg', 'https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/London-Eye....jpg', 'https://tempatwisataunik.com/wp-content/uploads/2016/03/Natural-History-Museum-1.jpg']
+  ),
+  Tourisms(name: 'Portobelo Market',
+      address: 'London, Inggris',
+      scedule: '06.00 -  07.00',
+      ticket:'200.000',
+      description: 'Portobello Road Market merupakan tempat wisata kedelapan yang dapat dikunjungi apabila Anda datang ke Inggris. Tempat ini merupakan tempat wisata yang berupa pasar dengan berbagai dagangan barang-barang antik.',
+      photo:'asset/img/portobelo_market.jpg',
+      imgurl: [
+        'https://tempatwisataunik.com/wp-content/uploads/2016/03/Portobello-Road-Market....jpg', 'https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/London-Eye....jpg', 'https://tempatwisataunik.com/wp-content/uploads/2016/03/Natural-History-Museum-1.jpg']
+  ),
+  Tourisms(name: 'Istana Kensington',
+      address: 'London, Inggris',
+      scedule: '06.00 -  07.00',
+      ticket:'200.000',
+      description: 'stana Kensington merupakan tempat wisata kesembilan yang dapat dikunjungi apabila Anda datang ke Inggris. Tempat ini merupakan istana yang dibentuk oleh generasi perempuan kerajaan dari Queen Mary ke Victoria, di mana baru-baru ini adalah Diana, Princess of Wales.',
+      photo:'asset/img/Istana_Kensington.jpg',
+      imgurl: [
+        'https://tempatwisataunik.com/wp-content/uploads/2016/03/Portobello-Road-Market....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/London-Eye....jpg', 'https://tempatwisataunik.com/wp-content/uploads/2016/03/Natural-History-Museum-1.jpg']
+  ),
+  Tourisms(name: 'Sungai thames',
+      address: 'London, Inggris',
+      scedule: '06.00 -  07.00',
+      description: 'Dengan mengunjungi tempat wisata yang satu ini Anda dapat melakukan sebuah pelayaran untuk melihat pemandangan dan kemegahan kota London dengan menggunakan kapal pesiar.',
+      ticket:'200.000',
+      photo:'asset/img/sungai_thames.jpg',
+      imgurl: [
+        'https://tempatwisataunik.com/wp-content/uploads/2016/03/Sungai-Thames.jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/London-Eye....jpg', 'https://tempatwisataunik.com/wp-content/uploads/2016/03/Natural-History-Museum-1.jpg']
+  ),
+  Tourisms(name: 'ZSL London',
+      address: 'London, Inggris',
+      scedule: '06.00 -  07.00',
+      description: 'ZSL London merupakan sebuah tempat wisata kebun binatang ilmiah yang dibuka sejak 1828 sehingga menjadikannya sebagai kebun binatang ilmiah tertua di dunia. Lokasi kebun binatang ini terletak di Regent’s Park, London, dengan 650 spesies binatang yang berbeda. Di sinilah Anda dapat merasakan pengalaman menyaksikan binatang besar, seperti jerapa dan gorilla.',
+      ticket:'200.000',
+      photo:'asset/img/zsl_london.jpg',
+      imgurl: [
+        'https://tempatwisataunik.com/wp-content/uploads/2016/03/ZSL-London.jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/London-Eye....jpg', 'https://tempatwisataunik.com/wp-content/uploads/2016/03/Natural-History-Museum-1.jpg']
+  ),
+  Tourisms(name: 'Hampton count palace',
+      address: 'London, Inggris',
+      scedule: '06.00 -  07.00',
+      description: 'Istana kerajaan ini terletak di barat daya London dengan berbagai perabotan istana menakjubkan yang dimilikinya, seperti permadani dan lukisan.',
+      ticket:'200.000',
+      photo:'asset/img/hampton_court_palace.jpg',
+      imgurl: [
+        'https://tempatwisataunik.com/wp-content/uploads/2016/03/Hampton-Court-Palace-1.jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/London-Eye....jpg', 'https://tempatwisataunik.com/wp-content/uploads/2016/03/Natural-History-Museum-1.jpg']
+  ),
+  Tourisms(name: 'Victoria and Albert Museum',
+      address: 'London, Inggris',
+      scedule: '06.00 -  07.00',
+      description: 'Tower Bridge merupakan sebuah struktur dan situs paling mengesankan yang berada di ibukota London, Inggris. Yang mana, Tower Bridge ini sudah berdiri sejak tahun 1894 yang sangat terkenal di dunia.',
+      ticket:'200.000',
+      photo:'asset/img/tower_bridge.jpg',
+      imgurl: [
+        'https://tempatwisataunik.com/wp-content/uploads/2016/03/Tower-Bridge....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/British-Museum....jpg','https://tempatwisataunik.com/wp-content/uploads/2016/03/London-Eye....jpg', 'https://tempatwisataunik.com/wp-content/uploads/2016/03/Natural-History-Museum-1.jpg'
+            '']
+  ),
+];
